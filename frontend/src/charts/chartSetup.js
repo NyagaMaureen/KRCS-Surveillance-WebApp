25ff00d9da0ddd64e57a6cd996823c8eb4fc444c
NyagaMaureen <maureen@goodpartnerske.org>
2026-09-23 12:08:11 +0300

    Remove legacy Vue frontend, fully superseded by React (frontend-react)

--- frontend/src/charts/chartSetup.js
+++
@@ -1,5 +0,0 @@
-import { Chart as ChartJS, registerables } from 'chart.js'
-
-ChartJS.register(...registerables)
-
-export const CHART_FONT = { family: 'Inter, sans-serif' }
--- frontend/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './main.css'
-import './charts/chartSetup'
-
-createApp(App).use(router).mount('#app')
